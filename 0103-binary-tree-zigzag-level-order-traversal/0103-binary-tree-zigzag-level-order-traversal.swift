25daeeeacd6e861c9c0f94e6636addd4c0bc5ee4
Woojin Song <woojin1900@gmail.com>
2025-09-15 14:12:17 +0900

    Time: 3 ms (2.22%), Space: 19.7 MB (68.15%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.swift
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root else { return [] }
+        
+        var res: [[Int]] = []
+        
+        var queue: [TreeNode] = [root]
+        var zigzag = false
+        
+        while !queue.isEmpty {
+            var level: [Int] = []
+            let count = queue.count
+            
+            for _ in 0..<count {
+                let node = queue.removeFirst()
+                
+                if zigzag {
+                    level.insert(node.val, at: 0)
+                } else {
+                    level.append(node.val)
+                }
+                
+                if let left = node.left {
+                    queue.append(left)
+                }
+                
+                if let right = node.right {
+                    queue.append(right)
+                }
+
+            }
+            
+            res.append(level)
+            zigzag.toggle()
+        }
+        
+        return res
+    }
+}
